9c4ec78822963b807495a8245df054ea48e99d03
joelazar <jozsef.gabor.lazar@gmail.com>
2017-10-01 12:58:47 +0200

    Readme.md modified, some mac part deleted from the repo
    Former-commit-id: e9f79e9cb67e2febe2e1956fcc3bffbff501742e

--- src/os/install/macos/vim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "${BASH_SOURCE[0]}")" \
-    && . "./utils.sh"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-brew_install "Vim" "vim --with-override-system-vi"
