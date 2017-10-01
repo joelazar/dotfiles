0744c6c35fb928e0094286e520ca0bdb9edd4b72
joelazar <jozsef.gabor.lazar@gmail.com>
2017-10-02 00:23:49 +0200

    some packages added

--- src/os/install/ubuntu/misc_tools.sh
+++ src/os/install/ubuntu/misc_tools.sh
@@ -9,5 +9,27 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n   Miscellaneous Tools\n\n"
 
 install_package "cURL" "curl"
-install_package "ShellCheck" "shellcheck"
+#install_package "ShellCheck" "shellcheck"
 install_package "xclip" "xclip"
+
+if ! package_is_installed "fluxgui"; then
+
+    add_ppa "nathan-renniewaldock/flux" \
+        || print_error "f.lux (add PPA)"
+
+    update &> /dev/null \
+        || print_error "f.lux (resync package index files)"
+
+    install_package "f.lux" "fluxgui"
+fi
+
+if ! package_is_installed "caffeine"; then
+
+    add_ppa "ppa:caffeine-developers/ppa" \
+        || print_error "caffeine (add PPA)"
+
+    update &> /dev/null \
+        || print_error "caffeine (resync package index files)"
+
+    install_package "caffeine" "caffeine"
+fi
