417094168bf7d4f750f3abf8f1d40ca2a0f07c37
joelazar <jozsef.gabor.lazar@gmail.com>
2017-10-02 21:55:17 +0200

    lets move dotfiles simply to HOME
    Former-commit-id: dde4530076e1f92a0cf01f4d40e75ab7865a5079

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -8,7 +8,7 @@ declare -r DOTFILES_UTILS_URL="https://raw.githubusercontent.com/$GITHUB_REPOSIT
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-declare dotfilesDirectory="$HOME/projects/dotfiles"
+declare dotfilesDirectory="$HOME/dotfiles"
 declare skipQuestions=false
 
 # ----------------------------------------------------------------------
