157887054915de6b2b2a5eaaa0f2d906fa54ce08
joelazar <jozsef.gabor.lazar@gmail.com>
2017-10-02 22:36:59 +0200

    corrections
    Former-commit-id: 43037659b4a1dc08afea6c490c696165ebc0c807

--- src/os/install/development.sh
+++ src/os/install/development.sh
@@ -2,11 +2,11 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")" \
     && . "../utils.sh" \
-    && . "utils.sh"
+    && . "./utils.sh"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-print_in_purple "\n   Git\n\n"
+print_in_purple "\n   Development\n\n"
 
 install_package "Git" "git"
 install_package "kdiff3" "kdiff3"
