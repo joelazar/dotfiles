7780f4e49eec69a99daae76f5bba2d160e36dddf
joelazar <jozsef.gabor.lazar@gmail.com>
2017-10-09 17:52:47 +0200

    corrections

--- src/os/install/browsers.sh
+++ src/os/install/browsers.sh
@@ -10,19 +10,19 @@ print_in_purple "\n   Browsers\n\n"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-if ! package_is_installed "google-chrome-unstable"; then
+if ! package_is_installed "google-chrome-stable"; then
 
     add_key "https://dl-ssl.google.com/linux/linux_signing_key.pub" \
-        || print_error "Chrome Canary (add key)"
+        || print_error "Chrome (add key)"
 
     add_to_source_list "[arch=amd64] https://dl.google.com/linux/deb/ stable main" "google-chrome.list" \
-        || print_error "Chrome Canary (add to package resource list)"
+        || print_error "Chrome (add to package resource list)"
 
     update &> /dev/null \
-        || print_error "Chrome Canary (resync package index files)"
+        || print_error "Chrome (resync package index files)"
 
 fi
 
-install_package "Chrome Canary" "google-chrome-stable"
+install_package "Chrome" "google-chrome-stable"
 
 install_package "Flash" "flashplugin-installer"
