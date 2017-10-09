d9763ddbdc58043e42662e1f977901eea8861199
joelazar <jozsef.gabor.lazar@gmail.com>
2017-10-09 20:54:39 +0200

    flux not needed for ubuntu 17.04

--- src/os/install/misc_tools.sh
+++ src/os/install/misc_tools.sh
@@ -11,17 +11,5 @@ print_in_purple "\n   Miscellaneous Tools\n\n"
 install_package "cURL" "curl"
 install_package "xclip" "xclip"
 install_package "Brotli" "brotli"
-
-if ! package_is_installed "fluxgui"; then
-
-    add_ppa "nathan-renniewaldock/flux" \
-        || print_error "f.lux (add PPA)"
-
-    update &> /dev/null \
-        || print_error "f.lux (resync package index files)"
-
-    install_package "f.lux" "fluxgui"
-
-fi
-
 install_package "caffeine" "caffeine"
+
