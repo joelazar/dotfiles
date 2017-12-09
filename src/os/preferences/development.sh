c7f1e4e09402eb8c5c0cfe2eb444b81f436379ce
jlazar - Lazar, Jozsef (Nokia - HU/Budapest) <jozsef.lazar@nokia.com>
2017-12-09 14:10:37 +0100

    not everything working yet and no need for some scripts for me

--- src/os/preferences/development.sh
+++ src/os/preferences/development.sh
@@ -9,10 +9,10 @@ print_in_purple "\n   Development configuration\n\n"
 
 execute "sudo systemctl enable docker" "Autostart docker"
 
-execute "getent group docker || (sudo groupadd docker && sudo gpasswd -a $USER docker)" \
+execute "getent group docker || (sudo groupadd docker && sudo usermod -aG docker $USER)" \
         "No sudo required for docker (logout required!)"
 
-execute "getent group pcap || (sudo groupadd pcap && sudo usermod -a -G pcap $USER \
+execute "getent group pcap || (sudo groupadd pcap && sudo usermod -aG pcap $USER \
          && sudo chgrp pcap /usr/sbin/tcpdump && sudo chmod 750 /usr/sbin/tcpdump \
          && sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump)" \
         "No sudo required for tcpdumping (logout required!)"
--- src/os/setup.sh
+++ src/os/setup.sh
@@ -266,22 +266,6 @@ main() {
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    if cmd_exists "git"; then
-
-        if [ "$(git config --get remote.origin.url)" != "$DOTFILES_ORIGIN" ]; then
-            ./initialize_git_repository.sh "$DOTFILES_ORIGIN"
-        fi
-
-        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-        if ! $skipQuestions; then
-            ./update_content.sh
-        fi
-
-    fi
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     if ! $skipQuestions; then
         ./restart.sh
     fi
