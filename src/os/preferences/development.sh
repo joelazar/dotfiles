f5fb60005024f78f70312bbf2450dbc2fcdb19f8
joelazar <jozsef.gabor.lazar@gmail.com>
2018-02-19 22:42:50 +0100

    small correction, in case tcpdump update lets update capabilites and permissions too, exercism completion added
    Former-commit-id: 4ac8931c707ef5bc62e1d1ff561810be8baf76b9

--- src/os/preferences/development.sh
+++ src/os/preferences/development.sh
@@ -12,8 +12,13 @@ execute "sudo systemctl enable docker" "Autostart docker"
 execute "getent group docker || (sudo groupadd docker && sudo usermod -aG docker $USER)" \
         "No sudo required for docker (logout required!)"
 
-execute "getent group pcap || (sudo groupadd pcap && sudo usermod -aG pcap $USER \
-         && sudo chgrp pcap /usr/sbin/tcpdump && sudo chmod 750 /usr/sbin/tcpdump \
-         && sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump)" \
-        "No sudo required for tcpdumping (logout required!)"
+execute "getent group pcap || (sudo groupadd pcap && sudo usermod -aG pcap $USER)" \
+        "Create pcap group (logout required!)"
 
+execute "sudo chgrp pcap /usr/sbin/tcpdump && sudo chmod 750 /usr/sbin/tcpdump \
+         && sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump" \
+        "No sudo required for tcpdumping"
+
+execute "if [ ! -e ~/.config/exercism/exercism_completion.bash ]; then mkdir -p ~/.config/exercism/ \
+         && curl http://cli.exercism.io/exercism_completion.bash > ~/.config/exercism/exercism_completion.bash; fi" \
+         "Exercism bash completions"
