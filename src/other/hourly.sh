405f87815c52bf9f9eebec9fbd0c519e002c7702
joelazar <jozsef.gabor.lazar@gmail.com>
2018-03-23 19:35:47 +0100

    if bing-wallpaper fails, then let's try without proxy :)

--- src/other/hourly.sh
+++ src/other/hourly.sh
@@ -1,1 +1,9 @@
 /usr/bin/bing-wallpaper en-US false
+
+if [[ $? -ne 0 ]]; then
+  unset http_proxy
+  unset https_proxy
+  unset HTTP_PROXY
+  unset HTTPS_PROXY
+  /usr/bin/bing-wallpaper en-US false
+fi
