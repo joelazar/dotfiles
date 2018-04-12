d36d5227e29d1f6c6291b0002de4aa2e474a4047
joelazar <jozsef.gabor.lazar@gmail.com>
2018-04-12 15:24:25 +0200

    some corrections
    Former-commit-id: 59e92f3ed591de996d3098e655c527e526d63157

--- src/other/hourly.sh
+++ src/other/hourly.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
+export XDG_SESSION_DESKTOP=xfce
 /usr/bin/bing-wallpaper en-US false
-
-if [[ $? -ne 0 ]]; then
-  unset http_proxy
-  unset https_proxy
-  unset HTTP_PROXY
-  unset HTTPS_PROXY
-  /usr/bin/bing-wallpaper en-US false
-fi
