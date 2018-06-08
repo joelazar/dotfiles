4ba631dd296c1a676fd008c364a0f790bb0cecf6
joelazar <jozsef.gabor.lazar@gmail.com>
2018-06-08 16:36:40 +0200

    penetration testing tools added, blackarch repo now installed
    Former-commit-id: f782a1ae582933a094fa3c30aeca2ecc43b33045

---
+++ src/os/install/penetration.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../utils.sh" \
+    && . "./utils.sh"
+
+install_blackarch_repo() {
+    if ! pacaur -Sg | grep -q "^blackarch" &> /dev/null; then
+        execute "curl -s https://blackarch.org/strap.sh | sudo bash" "install blackarch repo"
+    fi
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+print_in_purple "\n   Penetration testing\n\n"
+
+install_blackarch_repo
+install_package "wireshark" "wireshark-qt"
+install_package "nmap" "nmap"
+install_package "aircrack-ng suite" "blackarch/aircrack-ng"
+install_package "hydra" "blackarch/hydra"
+install_package "reaver" "blackarch/reaver"
+install_package "macchanger" "blackarch/macchanger"
+install_package "whatweb" "blackarch/whatweb"
+install_package "dirb" "blackarch/dirb"
+install_package "bettercap" "blackarch/bettercap"
