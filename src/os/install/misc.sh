5ab7782e16f9fd653dfbebec37f7993f702cc36c
joelazar <jozsef.gabor.lazar@gmail.com>
2018-06-16 11:52:07 +0200

    utilites.sh created
    Former-commit-id: 1a2bbb20b930c47924de9fa88a2f87f9490e1bdd

--- src/os/install/misc.sh
+++ src/os/install/misc.sh
@@ -8,23 +8,21 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Miscellaneous\n\n"
 
-install_package "bash-completion" "bash-completion"
+install_package "bing-wallpaper-git" "bing-wallpaper-git"
+install_package "brotli" "brotli"
 install_package "calibre" "calibre"
+install_package "colordiff" "colordiff"
 install_package "drive" "drive"
-install_package "firejail" "firejail"
-install_package "guake" "guake" # config needed later
-install_package "htop" "htop"
+install_package "hfsprogs" "hfsprogs"
 install_package "keePassX" "keepassx2"
-install_package "midnight commander" "mc"
 install_package "moreutils" "moreutils" # gem install rdoc was needed...
-install_package "net-tools" "net-tools"
 install_package "pidgin facebook plugin" "purple-facebook"
 install_package "pidgin" "pidgin"
 install_package "rdesktop" "rdesktop"
 install_package "redshift" "redshift"
 install_package "spotify" "spotify"
 install_package "sshfs" "sshfs"
-install_package "tcpdump" "tcpdump"
+install_package "sshpass" "sshpass"
 install_package "teamviewer" "teamviewer"
 install_package "tlp" "tlp"
 install_package "transmission" "transmission-qt"
@@ -32,4 +30,5 @@ install_package "virtualbox" "virtualbox" # not working yet
 install_package "vlc" "vlc"
 install_package "wine" "wine"
 install_package "wunderline" "wunderline" # config needed!
+install_package "xclip" "xclip"
 install_package "youtube-dl" "youtube-dl"
---
+++ src/os/install/utilities.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../utils.sh" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Utilities\n\n"
+
+install_package "bash-completion" "bash-completion"
+install_package "curl" "curl"
+install_package "firejail" "firejail"
+install_package "guake" "guake" # config needed later
+install_package "htop" "htop"
+install_package "midnight commander" "mc"
+install_package "net-tools" "net-tools"
+install_package "nethogs" "nethogs"
+install_package "tcpdump" "tcpdump"
+install_package "vnstat" "vnstat"
