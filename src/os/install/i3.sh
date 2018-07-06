57f720748fdd9349396b5f071742c0175a56ea6c
joelazar <jozsef.gabor.lazar@gmail.com>
2018-07-06 16:35:47 +0200

    i3.sh added
    Former-commit-id: 9b2cacdbbd37a992d9f7bb2e644378063a3c02f8

---
+++ src/os/install/i3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../utils.sh" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+    print_in_purple "\n   I3\n\n"
+
+    install_package "dunst" "dunst"
+    install_package "feh" "feh"
+    install_package "i3-gaps" "i3-gaps"
+    install_package "i3blocks" "i3blocks"
+    install_package "i3lock-color-git" "i3lock-color-git"
+    install_package "i3lock-fancy-git" "i3lock-fancy-git"
+    install_package "i3status" "i3status" # needed?
+    install_package "pa-applet" "pa-applet"
+    install_package "rofi" "rofi"
+    install_package "sysstat" "sysstat"
+    install_package "xautolock" "xautolock"
+    install_package "xorg-xbacklight" "xorg-xbacklight"
+    install_package "xprop" "xprop"
+
+}
+
+main
