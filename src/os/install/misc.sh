78e67387ff8f71a4dd968bbe1682325fb2532c09
joelazar <jozsef.gabor.lazar@gmail.com>
2018-09-30 21:02:17 +0200

    removed some unused packages
    Former-commit-id: be88fea1375dd2728cc57144f3c6e10bfed7f822

--- src/os/install/misc.sh
+++ src/os/install/misc.sh
@@ -8,23 +8,22 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Miscellaneous\n\n"
 
-install_package "atool" "atool"
-install_package "bing-wallpaper-git" "bing-wallpaper-git"
+# install_package "bing-wallpaper-git" "bing-wallpaper-git" # TODO - not working yet on i3, but I'm on it
 install_package "bleachbit" "bleachbit"
 install_package "brotli" "brotli"
 install_package "calibre" "calibre"
 install_package "colordiff" "colordiff"
 install_package "ctop" "ctop"
-install_package "drive" "drive"
+install_package "freerdp" "freerdp"
 install_package "hfsprogs" "hfsprogs"
 install_package "keePassX" "keepassx2"
 install_package "moreutils" "moreutils" # gem install rdoc was needed...
 install_package "ncdu" "ncdu"
-install_package "pidgin facebook plugin" "purple-facebook"
 install_package "pidgin" "pidgin"
 install_package "putty" "putty" # for puttygen ppk keys
 install_package "ranger" "ranger"
 install_package "rdesktop" "rdesktop"
+install_package "remmina" "remmina"
 install_package "redshift" "redshift"
 install_package "spotify" "spotify"
 install_package "sshfs" "sshfs"
