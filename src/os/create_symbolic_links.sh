bf888a171fd6a4ee749897e58858b81e633a7854
joelazar <jozsef.gabor.lazar@gmail.com>
2018-09-30 21:45:25 +0200

    compton update
    Former-commit-id: 5342d8025495d88fc615cf982ee5d35631030624

--- src/os/create_symbolic_links.sh
+++ src/os/create_symbolic_links.sh
@@ -7,6 +7,18 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 create_symlinks() {
  
+     create_symlink   "git/gitconfig"
+     create_symlink   "git/gitignore"
+     create_symlink   "i3/config" "$HOME/.config/i3/config"
+     create_symlink   "i3/i3blocks.conf" "$HOME/.config/i3/i3blocks.conf"
+     create_symlink   "nvim" "$HOME/.config/nvim"
+     create_symlink   "other/20-intel.conf" "/etc/X11/xorg.conf.d/20-intel.conf"
+     create_symlink   "other/30-touchpad.conf" "/etc/X11/xorg.conf.d/30-touchpad.conf"
+     create_symlink   "other/commands.py" "$HOME/.config/ranger/commands.py"
+     create_symlink   "other/compton.conf" "$HOME/.config/compton.conf"
+     create_symlink   "other/redshift.conf" "$HOME/.config/redshift.conf"
+     create_symlink   "other/sip_coloring.lua" "$HOME/.config/wireshark/plugins/sip_coloring.lua"
+     create_symlink   "services/xautolock.service" "/usr/lib/systemd/system/xautolock.service"
      create_symlink   "shell/bash_aliases"
      create_symlink   "shell/bash_autocomplete"
      create_symlink   "shell/bash_exports"
@@ -18,19 +30,8 @@ create_symlinks() {
      create_symlink   "shell/bashrc"
      create_symlink   "shell/curlrc"
      create_symlink   "shell/inputrc"
-     create_symlink   "git/gitconfig"
-     create_symlink   "git/gitignore"
      create_symlink   "tmux/tmux.conf"
-     create_symlink   "nvim" "$HOME/.config/nvim"
 #     create_symlink   "services/bing-wallpaper.service" "$HOME/.config/systemd/user/bing-wallpaper.service"
-     create_symlink   "services/xautolock.service" "/usr/lib/systemd/system/xautolock.service"
-     create_symlink   "i3/config" "$HOME/.config/i3/config"
-     create_symlink   "i3/i3blocks.conf" "$HOME/.config/i3/i3blocks.conf"
-     create_symlink   "other/sip_coloring.lua" "$HOME/.config/wireshark/plugins/sip_coloring.lua"
-     create_symlink   "other/commands.py" "$HOME/.config/ranger/commands.py"
-     create_symlink   "other/redshift.conf" "$HOME/.config/redshift.conf"
-     create_symlink   "other/20-intel.conf" "/etc/X11/xorg.conf.d/20-intel.conf"
-     create_symlink   "other/30-touchpad.conf" "/etc/X11/xorg.conf.d/30-touchpad.conf"
 
 }
 
