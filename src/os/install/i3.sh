be88fea1375dd2728cc57144f3c6e10bfed7f822
joelazar <jozsef.gabor.lazar@gmail.com>
2018-09-30 21:02:17 +0200

    removed some unused packages

--- src/os/install/i3.sh
+++ src/os/install/i3.sh
@@ -13,9 +13,7 @@ main() {
     install_package "feh" "feh"
     install_package "i3-gaps" "i3-gaps"
     install_package "i3blocks" "i3blocks"
-    install_package "i3lock-color-git" "i3lock-color-git"
     install_package "i3lock-fancy-git" "i3lock-fancy-git"
-    install_package "i3status" "i3status" # needed?
     install_package "parcellite" "parcellite"
     install_package "pa-applet" "pa-applet"
     install_package "rofi" "rofi"
