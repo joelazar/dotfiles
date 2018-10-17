bdfada1b10d70a1e7d5ad7918a3bb6c7d2ac145e
joelazar <jozsef.gabor.lazar@gmail.com>
2018-10-17 11:20:17 +0200

    some neoformat options added to nvim config, clang-format file added and linked to /home/jlazar

--- src/other/backup.sh
+++ src/other/backup.sh
@@ -1,4 +1,10 @@
-restic -r ~/OneDrive/backup backup ~/.ssh ~/Pictures ~/joe_stuff ~/notes ~/university
+restic -r ~/OneDrive/backup backup ~/.ssh \
+                                   ~/Documents \
+                                   ~/Pictures \
+                                   ~/private \
+                                   ~/logs \
+                                   ~/notes
+
 # restic -r ~/OneDrive/backup snapshots 
 # restic -r ~/OneDrive/backup forget snapshothash
 # restic -r ~/OneDrive/backup prune 
