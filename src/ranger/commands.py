4ce95413512afe2f5e2eff6a9842a71b75d121e1
joelazar <jozsef.gabor.lazar@gmail.com>
2019-05-23 20:06:18 +0200

    ag rewritten to rg, fzf command added to ranger
    Former-commit-id: 7dadf7fbdfd6d2207f99d35adebbfdf48abc0c0e

--- src/ranger/commands.py
+++ src/ranger/commands.py
@@ -31,9 +31,9 @@ class extracthere(Command):
         else:
             descr = "extracting files from: " + os.path.basename(
                 one_file.dirname)
-        obj = CommandLoader(
-            args=['aunpack'] + au_flags + [f.path for f in copied_files],
-            descr=descr)
+        obj = CommandLoader(args=['aunpack'] + au_flags +
+                            [f.path for f in copied_files],
+                            descr=descr)
 
         obj.signal_bind('after', refresh)
         self.fm.loader.add(obj)
@@ -57,8 +57,10 @@ class compress(Command):
         au_flags = parts[1:]
 
         descr = "compressing files in: " + os.path.basename(parts[1])
-        obj = CommandLoader(args=['apack'] + au_flags + \
-                [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr)
+        obj = CommandLoader(
+            args=['apack'] + au_flags +
+            [os.path.relpath(f.path, cwd.path) for f in marked_files],
+            descr=descr)
 
         obj.signal_bind('after', refresh)
         self.fm.loader.add(obj)
@@ -73,23 +75,19 @@ class compress(Command):
         ]
 
 
-class ag(Command):
-    """:ag 'regex'
+class rg(Command):
+    """:rg 'regex'
     Looks for a string in all marked paths or current dir
     """
     editor = os.getenv('EDITOR') or 'vim'
-    acmd = 'ag --smart-case --group --nocolor --hidden'  # --search-zip
+    acmd = 'rg --smart-case --hidden'
     qarg = re.compile(r"""^(".*"|'.*')$""")
     patterns = []
-
-    # THINK:USE: set_clipboard on each direct ':ag' search? So I could find in vim easily
 
     def _sel(self):
         d = self.fm.thisdir
         if d.marked_items:
             return [f.relative_path for f in d.marked_items]
-        # WARN: permanently hidden files like .* are searched anyways
-        #   << BUG: files skipped in .agignore are grep'ed being added on cmdline
         if d.temporary_filter and d.files_all and (len(d.files_all) != len(
                 d.files)):
             return [f.relative_path for f in d.files]
@@ -97,28 +95,23 @@ class ag(Command):
 
     def _arg(self, i=1):
         if self.rest(i):
-            ag.patterns.append(self.rest(i))
-        return ag.patterns[-1] if ag.patterns else ''
-
-    def _quot(self, patt):
-        return patt if ag.qarg.match(patt) else shell_quote(patt)
+            rg.patterns.append(self.rest(i))
+        return rg.patterns[-1] if rg.patterns else ''
 
     def _bare(self, patt):
-        return patt[1:-1] if ag.qarg.match(patt) else patt
+        return patt[1:-1] if rg.qarg.match(patt) else patt
 
-    def _aug_vim(self, iarg, comm='Ag'):
+    def _aug_vim(self, iarg, comm='Rg'):
         if self.arg(iarg) == '-Q':
             self.shift()
             comm = 'sil AgSet def.e.literal 1|' + comm
-        # patt = self._quot(self._arg(iarg))
-        patt = self._arg(iarg)  # No need to quote in new ag.vim
-        # FIXME:(add support)  'AgPaths' + self._sel()
+        patt = self._arg(iarg)
         cmd = ' '.join([comm, patt])
-        cmdl = [ag.editor, '-c', cmd, '-c', 'only']
+        cmdl = [rg.editor, '-c', cmd, '-c', 'only']
         return (cmdl, '')
 
     def _aug_sh(self, iarg, flags=[]):
-        cmdl = ag.acmd.split() + flags
+        cmdl = rg.acmd.split() + flags
         if iarg == 1:
             import shlex
             cmdl += shlex.split(self.rest(iarg))
@@ -129,9 +122,7 @@ class ag(Command):
                 self.shift()
                 cmdl.append(opt)
                 opt = self.arg(iarg)
-            # TODO: save -Q/-w into ag.patterns =NEED rewrite plugin to join _aug*()
-            patt = self._bare(
-                self._arg(iarg))  # THINK? use shlex.split() also/instead
+            patt = self._bare(self._arg(iarg))
             cmdl.append(patt)
         if '-g' not in flags:
             cmdl += self._sel()
@@ -139,9 +130,9 @@ class ag(Command):
 
     def _choose(self):
         if self.arg(1) == '-v':
-            return self._aug_vim(2, 'Ag')
+            return self._aug_vim(2, 'Rg')
         elif self.arg(1) == '-g':
-            return self._aug_vim(2, 'sil AgView grp|Ag')
+            return self._aug_vim(2, 'sil RgView grp|Rg')
         elif self.arg(1) == '-l':
             return self._aug_sh(2, ['--files-with-matches', '--count'])
         elif self.arg(1) == '-p':  # paths
@@ -162,35 +153,49 @@ class ag(Command):
         else:
             return out[:-1].decode('utf-8').splitlines()
 
-    # DEV
-    # NOTE: regex becomes very big for big dirs
-    # BAD: flat ignores 'filter' for nested dirs
     def _filter(self, lst, thisdir):
-        # filter /^rel_dir/ on lst
-        # get leftmost path elements
-        # make regex '^' + '|'.join(re.escape(nm)) + '$'
-        thisdir.temporary_filter = re.compile(file_with_matches)
         thisdir.refilter()
 
         for f in thisdir.files_all:
             if f.is_directory:
-                # DEV: each time filter-out one level of files from lst
                 self._filter(lst, f)
 
     def execute(self):
         cmd, flags = self._choose()
-        # self.fm.notify(cmd)
-        # TODO:ENH: cmd may be [..] -- no need to shell_escape
         if self.arg(1) != '-r':
             self.fm.execute_command(cmd, flags=flags)
         else:
             self._filter(self._catch(cmd))
 
     def tab(self):
-        # BAD:(:ag <prev_patt>) when input alias ':agv' and then <Tab>
-        #   <= EXPL: aliases expanded before parsing cmdline
         cmd = self.arg(0)
         flg = self.arg(1)
         if flg[0] == '-' and flg[1] in 'flvgprw':
             cmd += ' ' + flg
-        return ['{} {}'.format(cmd, p) for p in reversed(ag.patterns)]
+        return ['{} {}'.format(cmd, p) for p in reversed(rg.patterns)]
+
+
+class fzf(Command):
+    """
+    :fzf
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+
+    def execute(self):
+        import subprocess
+        import os.path
+        fzf = self.fm.execute_command("fzf +m",
+                                      universal_newlines=True,
+                                      stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
