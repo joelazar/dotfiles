2e72b4a6f8ddc2d91ac22e4cd293f253863c8837
joelazar <jozsef.gabor.lazar@gmail.com>
2019-06-08 17:52:20 +0200

    removed some unused packages
    Former-commit-id: c926654e4cb1186dba6c5d92ad951a83bfaad153

--- src/os/install/aur.sh
+++ src/os/install/aur.sh
@@ -12,19 +12,15 @@ main() {
     ask_for_confirmation "Do you want really want to install AUR packages now? Warning, they will be installed with noconfirm flag."
 
     if answer_is_yes; then
-        install_package "caffeine-ng" "caffeine-ng"
         install_package "ctop" "ctop"
         install_package "exercism" "exercism-cli"
         install_package "heroku-cli" "heroku-cli"
         install_package "hfsprogs" "hfsprogs"
         install_package "realvnc-vnc-viewer" "realvnc-vnc-viewer"
         install_package "restic-git" "restic-git"
-        install_package "pipdeptree" "pipdeptree"
-        install_package "signal" "signal"
         install_package "spotify" "spotify"
         install_package "teamviewer" "teamviewer"
         install_package "widevine plugin for Chromium" "chromium-widevine"
-        install_package "wunderline" "wunderline"
     fi
 
 }
