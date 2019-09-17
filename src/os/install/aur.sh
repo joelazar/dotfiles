0f105749a787f640c325d90c3557ffc349e84e07
joelazar <jozsef.gabor.lazar@gmail.com>
2019-09-17 18:28:37 +0200

    really quick cleanup, use chrome again as a primary browser, sorry fox
    Former-commit-id: fe14e3df0f046f5021abab2a9d22bd83deb25c3c

--- src/os/install/aur.sh
+++ src/os/install/aur.sh
@@ -12,15 +12,12 @@ main() {
     ask_for_confirmation "Do you want really want to install AUR packages now? Warning, they will be installed with noconfirm flag."
 
     if answer_is_yes; then
-        install_package "ctop" "ctop"
         install_package "exercism" "exercism-bin"
-        install_package "heroku-cli" "heroku-cli"
         install_package "hfsprogs" "hfsprogs"
-        install_package "realvnc-vnc-viewer" "realvnc-vnc-viewer"
+        install_package "google chrome" "google-chrome"
         install_package "restic-git" "restic-git"
         install_package "spotify" "spotify"
         install_package "teamviewer" "teamviewer"
-        install_package "widevine plugin for Chromium" "chromium-widevine"
     fi
 
 }
