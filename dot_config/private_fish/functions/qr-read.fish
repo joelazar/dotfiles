280e6aa88138526357af9e3c22d2cb1f0e299a39
joelazar <jozsef.gabor.lazar@gmail.com>
2022-02-11 16:37:54 +0100

    :memo: add doc to fish functions
    Former-commit-id: e58b9c050a7a5d2560046d28bdaf062b7e20f0e5

--- dot_config/private_fish/functions/qr-read.fish
+++ dot_config/private_fish/functions/qr-read.fish
@@ -1,4 +1,4 @@
-function qr-read
+function qr-read --description 'read qr encoded data from a file'
     set img $argv[1]
     cat $img | zbarimg -
 end
--- dot_config/private_fish/functions/qr-write.fish
+++ dot_config/private_fish/functions/qr-write.fish
@@ -1,4 +1,4 @@
-function qr-write
+function qr-write --description 'write qr encoded data from a file'
     set file $argv[1]
     cat $file | qrencode -t UTF8
 end
