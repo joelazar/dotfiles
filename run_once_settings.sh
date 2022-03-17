d40bd632c1e38a53192f61788568414ca2e5e91a
joelazar <jozsef.gabor.lazar@gmail.com>
2022-03-17 12:14:24 +0100

    :fire: firejail is not working for these programs
    Former-commit-id: e64b66d50184071663eb932f2010c04e5aec1513

--- run_once_settings.sh
+++ run_once_settings.sh
@@ -32,6 +32,8 @@ execute "if [ ! -d $HOME/.config/nvim ]; then git clone git@github.com:joelazar/
 
 execute "sudo firecfg" "Firejail auto config"
 
+execute "sudo rm /usr/local/bin/gcloud /usr/local/bin/code" "These firejail profiles are not working yet"
+
 execute "sudo timedatectl set-ntp true" "Turn on ntp"
 
 execute "sudo systemctl enable NetworkManager.service" "Turn on nm"
