57433f05670c9eb3f5963b2fd522b9300375bef8
joelazar <jozsef.gabor.lazar@gmail.com>
2022-10-19 11:07:05 +0200

    :sparkles: install deno, peek preview no focus
    Former-commit-id: eab97a8e86e250d997aaa9c867b3585cf077b34f

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -67,6 +67,7 @@ install_package "minikube"
 print_in_purple "Languages\n"
 
 install_package "clang"
+install_package "deno"
 install_package "gcc"
 install_package "go"
 install_package "jdk-openjdk"
