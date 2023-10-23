3d64efcb54c74f38bbd8fa1a9218798784414f4f
joelazar <jozsef.gabor.lazar@gmail.com>
2023-10-23 16:15:01 +0200

    :rocket: add bootstrap.sh for github codespaces

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This file is only used to initialize the Github codespaces environment.
+
+set -e
+
+# Install requirements
+
+sudo apt update
+sudo apt -y install software-properties-common
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
+sudo apt update
+
+sudo apt install -y ripgrep fd-find neovim git curl golang rust-all npm python
+
+mkdir -p ~/.config/kitty
+
+git clone https://github.com/joelazar/nvim-config.git ~/.config/nvim
