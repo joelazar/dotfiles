5225210e7c336e0d1a251f831a799d3593c992cc
joelazar <jozsef.gabor.lazar@gmail.com>
2024-01-14 17:28:44 +0100

    :fire: clean up ipython config

--- dot_ipython/profile_default/ipython_config.py
+++ dot_ipython/profile_default/ipython_config.py
@@ -2,19 +2,15 @@ import sys
 
 c.InteractiveShellApp.ignore_cwd = True
 c.TerminalIPythonApp.display_banner = False
-c.InteractiveShell.colors = 'Linux'
-c.InteractiveShell.xmode = 'Minimal'
-c.TerminalInteractiveShell.colors = 'Neutral'
+c.InteractiveShell.colors = "Linux"
+c.InteractiveShell.xmode = "Minimal"
+c.TerminalInteractiveShell.colors = "Neutral"
 c.TerminalInteractiveShell.confirm_exit = False
-c.TerminalInteractiveShell.display_completions = 'readlinelike'
-c.TerminalInteractiveShell.editing_mode = 'vi'
-c.TerminalInteractiveShell.editor = 'nvim'
+c.TerminalInteractiveShell.display_completions = "readlinelike"
+c.TerminalInteractiveShell.editing_mode = "vi"
+c.TerminalInteractiveShell.editor = "nvim"
 c.TerminalInteractiveShell.extra_open_editor_shortcuts = True
 c.TerminalInteractiveShell.prompt_includes_vi_mode = False
 c.TerminalInteractiveShell.term_title = False
-c.TerminalInteractiveShell.term_title_format = 'IPython ' + sys.version.split(' ')[0]
+c.TerminalInteractiveShell.term_title_format = "IPython " + sys.version.split(" ")[0]
 c.TerminalInteractiveShell.true_color = True
-c.InteractiveShellApp.exec_lines = [
-    'from matplotlib import pyplot as plt',
-    'import numpy as np',
-]
