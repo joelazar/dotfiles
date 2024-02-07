eeadcc66c294cf980ee72e50dbf8249bffbaa11d
joelazar <jozsef.gabor.lazar@gmail.com>
2024-02-07 01:04:09 +0100

    :sparkles: cleanup and get install script a bit better shape

--- run_once_settings.sh
+++ run_once_settings.sh
@@ -18,19 +18,6 @@ ask_for_sudo
 execute "(getent group docker | grep $USER) || sudo usermod -aG docker $USER" \
 	"No sudo required for docker users (logout required!)"
 
-# execute "(getent group input | grep $USER) || sudo usermod -aG input $USER" \
-# 	"Add input group to user (for touchpad gestures)"
-
-# execute "getent group pcap || (sudo groupadd pcap && sudo usermod -aG pcap $USER)" \
-# 	"Create pcap group (logout required!)"
-
-# execute "sudo chgrp pcap /usr/sbin/tcpdump && sudo chmod 750 /usr/sbin/tcpdump \
-#          && sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump" \
-# 	"No sudo required for tcpdumping"
-
-# execute "(getent group vboxusers | grep $USER) || sudo usermod -aG vboxusers $USER" \
-# 	"User added to vboxusers group (logout required!)"
-
 if [ "$SHELL" != "/usr/bin/fish" ]; then
 	chsh -s /usr/bin/fish
 	print_result $? "Set shell to fish"
@@ -38,37 +25,4 @@ fi
 
 execute "if [ ! -d $HOME/.config/nvim ]; then git clone git@github.com:joelazar/nvim-config.git $HOME/.config/nvim; fi;" "Clone neovim config repo"
 
-# execute "sudo firecfg" "Firejail auto config"
-# 
-# execute "sudo timedatectl set-ntp true" "Turn on ntp"
-# 
-# execute "sudo systemctl enable NetworkManager.service" "Turn on nm"
-# 
-# execute "sudo systemctl enable NetworkManager-dispatcher.service" "Turn on nm-dispatcher"
-# 
-# execute "sudo systemctl disable NetworkManager-wait-online.service" "Turn off nm-wait-online"
-# 
-# execute "sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket" "Turn off rfkill"
-# 
-# execute "sudo systemctl enable tlp.service" "Turn on tlp"
-# 
-# execute "sudo systemctl enable systemd-oomd.service" "Turn on oomd"
-# 
-# execute "systemctl --user enable pipewire-pulse" "Turn on pipewire pulseaudio server"
-# 
-# execute "systemctl --user enable libinput-gestures.service" "Turn on touchpad gestures service"
-# 
-# execute "sudo systemctl enable bluetooth.service" "Turn on bluetooth"
-# 
-# execute "sudo systemctl enable docker.service" "Turn on docker"
-# 
-# execute "sudo systemctl enable iptables.service" "Turn on iptables"
-# 
-# execute "sudo systemctl enable ip6tables.service" "Turn on ip6tables"
-
-execute "fish_config theme save \"Catppuccin Mocha\"" "Set fish theme"
-
-# execute "sudo sed -i '/Color$/s/^#//g' /etc/pacman.conf && \
-#          sudo sed -i '/TotalDownload$/s/^#//g' /etc/pacman.conf && \
-#          sudo sed -i '/CheckSpace$/s/^#//g' /etc/pacman.conf && \
-#          sudo sed -i '/VerbosePkgLists$/s/^#//g' /etc/pacman.conf" "Update pacman.conf"
+execute "fish -c 'fish_config theme save \"Catppuccin Mocha\"'" "Set fish theme"
