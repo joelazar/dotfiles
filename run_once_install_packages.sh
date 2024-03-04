b15d69028a821289d412028e2bd0cf9d25c3afca
joelazar <jozsef.gabor.lazar@gmail.com>
2024-03-04 19:51:44 +0100

    :tada: starting to migrate over to mac

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -19,171 +19,35 @@ fi
 
 ask_for_sudo
 
-# TODO: enable rpmfusion repos
-# $ sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# TODO: install homebrew
+#
+# set fish as default
+# sudo sh -c 'echo /opt/homebrew/bin/fish >> /etc/shells'
+# chsh -s /opt/homebrew/bin/fish
+#
 # install chezmoi -->
-# sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
-# enable flathub
-# flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# /opt/homebrew/bin/brew install chezmoi
 
-print_in_purple "Browsers\n"
-
-install_package "firefox"
-install_package "chromium"
-
-print_in_purple "Basics\n"
-
-install_package "bat"
-install_package "docker"
-install_package "eza"
-install_package "fd-find"
-install_package "git"
-install_package "git-lfs"
-install_package "htop"
-install_package "httpie"
-install_package "neovim"
-install_package "nnn"
-install_package "python"
-install_package "ripgrep"
-install_package "sd"
-install_package "zoxide"
-
-print_in_purple "Infrastructure\n"
-
-install_package "docker-compose"
-install_package "kubernetes-client"
-
-print_in_purple "Languages\n"
-
-install_package "clang"
-install_package "g++"
-install_package "go"
-install_package "nodejs"
-install_package "rust"
+# brew bundle install
 
 # TODO: add mojo
 
 print_in_purple "Package managers\n"
 
-install_package "cargo"
-install_package "luarocks"
-install_package "npm"
-install_package "python-pip"
-install_package "python-poetry"
-install_package "yarnpkg"
-
-print_in_purple "Interpreters\n"
-
-install_package "ipython"
-
-print_in_purple "Misc\n"
-
-install_package "python3-neovim"
-install_package "texlive-latex"
-
-print_in_purple "Development tools\n"
-
-install_package "curl"
-install_package "direnv"
-install_package "exercism"
-install_package "git-delta"
-install_package "gh"
-install_package "hugo"
-install_package "jupyterlab"
-install_package "postgresql"
-install_package "pgcli"
-
-print_in_purple "Multimedia\n"
-
-install_package "mpv"
-
-print_in_purple "Miscellaneous\n"
-
-install_package "calibre"
-install_package "lftp"
-install_package "ncdu"
-install_package "qalculate"
-install_package "qrencode"
-install_package "tealdeer"
-install_package "tokei"
-install_package "translate-shell"
-install_package "transmission-cli"
+pyenv
 
 install_nnn_plugins
 
-print_in_purple "Fonts\n"
-
-# TODO: well, this one is not nerd font patched
-install_package "fira-code-fonts"
-
-print_in_purple "Shell\n"
-
-install_package "fish"
-# TODO: install_package "fisher"
-# execute "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher" "Install fisher"
-install_package "kitty"
-
-print_in_purple "Utilities\n"
-
-install_package "age"
-install_package "btop"
-install_package "duf"
-install_package "fastfetch"
-install_package "ffmpeg"
-install_package "ffmpegthumbnailer"
-install_package "fzf"
-install_package "jq"
-install_package "jc"
-# NOTE: needed for cargo-update
-install_package "openssl-devel"
-install_package "sad"
-
-# TODO: add mullvad repo
-# sudo dnf config-manager --add-repo https://repository.mullvad.net/rpm/stable/mullvad.repo
-install_package "mullvad-vpn"
-install_package "newsboat"
-install_package "rclone"
-install_package "restic"
-install_package "wget"
-
-print_in_purple "Asahi specific\n"
-
-install_package "asahi-nvram"
-
-# NOTE: not working yet
-# install_package "bottles"
-# install_package "tailscale"
-# install_package "solo2-cli"
-# install_package "wine"
-
-# TODO: google-cloud-cli installed with:
-# curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-462.0.1-linux-x86_64.tar.gz
-# NOTE: python-grpcio is required for some google-cloud-cli commands
-install_package "python3-grpcio"
-
 print_in_purple "NPM packages\n"
 
-install_npm_package "gitmoji-cli"
 install_npm_package "npm-check"
 install_npm_package "yalc"
 
 print_in_purple "Go packages\n"
 
 install_go_package "github.com/cosmos72/gomacro@latest" "gomacro"
-install_go_package "github.com/charmbracelet/gum@latest" "gum"
-install_go_package "github.com/derailed/k9s@latest" "k9s"
-install_go_package "github.com/jesseduffield/lazygit@latest" "lazygit"
 
 print_in_purple "Rust packages\n"
-
-install_rust_package "atuin"
-install_rust_package "cargo-update"
-# TODO: deno not working yet
-# install_rust_package "deno"
-install_rust_package "fnm"
-install_rust_package "starship"
-install_rust_package "topgrade"
-install_rust_package "watchexec-cli"
 
 print_in_purple "GH plugins\n"
 
@@ -191,8 +55,3 @@ print_in_purple "GH plugins\n"
 
 install_gh_plugin "dlvhdr/gh-dash"
 install_gh_plugin "github/gh-copilot"
-
-# TODO: install flatpak apps
-# install_rust_package "ncspot"
-# install_package "obsidian"
-# install_package "armcord"
