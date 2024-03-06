af8caaaf73b9626c2af918599c11d63b0f685687
joelazar <jozsef.gabor.lazar@gmail.com>
2024-03-06 16:48:31 +0100

    :sparkles: update run_once_settings.sh with mac specific configurations

--- run_once_settings.sh
+++ run_once_settings.sh
@@ -23,18 +23,27 @@ fi
 
 execute "if [ ! -d $HOME/.config/nvim ]; then git clone git@github.com:joelazar/nvim-config.git $HOME/.config/nvim; fi;" "Clone neovim config repo"
 
+# TODO: do not set theme if it was already set
 execute "fish -c 'fish_config theme save \"Catppuccin Mocha\"'" "Set fish theme"
 
-# TODO: turn off boot sound
-# sudo asahi-nvram write system:StartupMute=%01
+execute "defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false" "Disable tooltip when holding key"
 
-# TODO: set obsidian to use wayland
-# flatpak override --user --socket=wayland md.obsidian.Obsidian
+execute "defaults write NSGlobalDomain KeyRepeat -float 1.0" "Set a blazingly fast keyboard repeat rate"
 
-# TODO: set armcord to use wayland
-# flatpak override --user --socket=wayland xyz.armcord.ArmCord
+execute "defaults write NSGlobalDomain InitialKeyRepeat -int 12" "Set a shorter Delay until key repeat"
 
-# TODO: swap ctrl and function keys
-# echo "options hid_apple swap_fn_leftctrl=1" > /etc/modprobe.d/keyboard.conf
-# regenerate initramfs
-# sudo dracut --regenerate-all --force
+execute "sudo nvram SystemAudioVolume=\" \"" "Disable the sound effects on boot"
+
+execute "defaults write NSGlobalDomain AppleShowAllExtensions -bool true" "Show all filename extensions"
+
+execute "defaults write com.apple.dock autohide -bool true" "Automatically hide and show the Dock"
+
+execute "defaults write com.apple.dock autohide-delay -float 0" "Remove the auto-hiding Dock delay"
+
+execute "defaults write com.apple.dock autohide-time-modifier -float 0" "Remove the animation when hiding/showing the Dock"
+
+# swap ctrl and function keys
+# execute "hidutil property --set '{\"UserKeyMapping\":[{\"HIDKeyboardModifierMappingSrc\":0x7000000E0,\"HIDKeyboardModifierMappingDst\":0x7000000E1},{\"HIDKeyboardModifierMappingSrc\":0x7000000E1,\"HIDKeyboardModifierMappingDst\":0x7000000E0}]}'" "Swap ctrl and function keys"
+
+# map caps lock to escape
+# execute "hidutil property --set '{\"UserKeyMapping\":[{\"HIDKeyboardModifierMappingSrc\":0x700000039,\"HIDKeyboardModifierMappingDst\":0x700000029}]}'" "Map caps lock to escape"
