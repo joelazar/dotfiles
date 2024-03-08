fa76997a28ab8ec321fbb3f2c85946448e72f3b8
joelazar <jozsef.gabor.lazar@gmail.com>
2024-03-08 12:20:44 +0100

    :fire: remove unused script and update wl-copy to pbcopy

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -40,7 +40,7 @@ set -gx MODULAR_HOME $HOME/.modular
 
 # FZF options
 set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git --exclude node_modules'
-set -gx FZF_DEFAULT_OPTS '--height 50% --layout=reverse --border --info=inline --marker="*" --bind "ctrl-y:execute(echo {+} | wl-copy)" --bind "ctrl-a:select-all" --bind "?:toggle-preview"'
+set -gx FZF_DEFAULT_OPTS '--height 50% --layout=reverse --border --info=inline --marker="*" --bind "ctrl-y:execute(echo {+} | pbcopy)" --bind "ctrl-a:select-all" --bind "?:toggle-preview"'
 set fzf_history_opts --sort --exact --history-size=30000
 set fzf_fd_opts --hidden --follow --exclude=.git
 set fzf_preview_dir_cmd eza -la --git --group-directories-first --icons --color=always
