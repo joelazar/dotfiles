11b848ef4defe16dd0fbbc025b4691e7a5f73a91
joelazar <jozsef.gabor.lazar@gmail.com>
2024-05-25 14:40:03 +0200

    :wrench: update run_once_settings.sh script, configure almost everything there

--- run_once_settings.sh
+++ run_once_settings.sh
@@ -23,10 +23,9 @@ fi
 
 execute "if [ ! -d $HOME/.config/nvim ]; then git clone git@github.com:joelazar/nvim-config.git $HOME/.config/nvim; fi;" "Clone neovim config repo"
 
-# TODO: do not set theme if it was already set
-execute "fish -c 'fish_config theme save \"Catppuccin Mocha\"'" "Set fish theme"
+execute "fish -c 'echo y | fish_config theme save Catppuccin\ Mocha'" "Set fish theme"
 
-# NOTE: settings based on https://mac-key-repeat.zaymon.dev/
+# settings based on https://mac-key-repeat.zaymon.dev/
 execute "defaults write NSGlobalDomain KeyRepeat -int 1" "Keyboard: Set 15 ms key repeat"
 
 execute "defaults write NSGlobalDomain InitialKeyRepeat -int 13" "Keyboard: Set 195 ms initial delay"
@@ -47,46 +46,47 @@ execute "sudo spctl --master-disable" "Allow apps downloaded from anywhere"
 
 execute "defaults write com.apple.LaunchServices LSQuarantine -bool false" "Disable the 'Are you sure you want to open this application?' dialog"
 
-# TODO:
-# swap ctrl and function keys
-# execute "hidutil property --set '{\"UserKeyMapping\":[{\"HIDKeyboardModifierMappingSrc\":0x7000000E0,\"HIDKeyboardModifierMappingDst\":0x7000000E1},{\"HIDKeyboardModifierMappingSrc\":0x7000000E1,\"HIDKeyboardModifierMappingDst\":0x7000000E0}]}'" "Swap ctrl and function keys"
+execute "defaults write -g com.apple.keyboard.fnState -bool true" "Function keys as standard function keys"
 
-# TODO:
-# use f1, f2, etc. as standard function keys by default
+execute 'defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 64 "{enabled = 0;}"' "Disable Spotlight keyboard shortcut for Show Spotlight search"
 
-# TODO:
-# do not change input source for ctrl+space
+execute 'defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 65 "{enabled = 0;}"' "Disable Spotlight keyboard shortcut for Finder search window"
 
-# TODO:
-# turn off spotlight keyboard shortcuts, raycast will use that instead
+execute 'defaults write NSGlobalDomain AppleActionOnDoubleClick -string "Maximize"' "Double-click a window's title bar to minimize"
 
-# TODO:
-# double clicking on title bar should zoom
+execute "defaults write com.apple.screencapture location ~/Pictures/screenshots/" "Save screenshots to the Pictures/screenshots directory"
 
-# TODO:
-# setup night shift
+ask_for_confirmation "Do you want to setup hostname? (y/n)"
+if answer_is_yes; then
+  ask "Enter new hostname: "
+  sudo scutil --set HostName "$REPLY"
+  sudo scutil --set LocalHostName "$REPLY"
+  sudo scutil --set ComputerName "$REPLY"
+  dscacheutil -flushcache
+  echo And you need to reboot
+fi
 
-# TODO:
-# set default location for screenshots
-
-# TODO:
-# set hostname
-# sudo scutil --set HostName <new host name>
-# sudo scutil --set LocalHostName <new host name>
-# sudo scutil --set ComputerName <new name>
-# dscacheutil -flushcache
-# reboot
-
-# TODO:
 # disable wakeup on bluetooth
 # https://apple.stackexchange.com/questions/431812/how-can-i-stop-a-bluetooth-keyboard-from-waking-my-macbook-pro/437396#437396
-# brew install sleepwatcher blueutil
-# echo "$(which blueutil) -p 0" > ~/.sleep
-# echo "$(which blueutil) -p 1" > ~/.wakeup
-# chmod 755 ~/.sleep ~/.wakeup
-# brew services restart sleepwatcher
+if [ ! -f ~/.sleep ] || [ ! -f ~/.wakeup ]; then
+  echo "$(which blueutil) -p 0" >~/.sleep
+  echo "$(which blueutil) -p 1" >~/.wakeup
+  chmod 755 ~/.sleep ~/.wakeup
+  brew services restart sleepwatcher
+  print_result $? "Setup sleepwatcher"
+else
+  print_success "Setup sleepwatcher"
+fi
 
-# TODO:
-# setup docker buildx correctly
-# mkdir -p ~/.docker/cli-plugins
-# ln -sf /opt/homebrew/opt/docker-buildx/bin/docker-buildx ~/.docker/cli-plugins/docker-buildx
+if [ -f ~/.docker/cli-plugins/docker-buildx ]; then
+  print_success "Docker buildx configured"
+else
+  mkdir -p ~/.docker/cli-plugins
+  ln -sf /opt/homebrew/opt/docker-buildx/bin/docker-buildx ~/.docker/cli-plugins/docker-buildx
+  print_result $? "Docker buildx configured"
+fi
+
+# NOTE: the following configuration currently only possible to configure manually
+# swap ctrl and function keys
+# do not change input source for ctrl+space
+# setup night shift
