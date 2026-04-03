bf4b09cf68e75dac8b38f95de1874dde0fa19559
joelazar <jozsef.gabor.lazar@gmail.com>
2026-04-03 17:43:25 +0200

    :sparkles: add default-tools extension

---
+++ dot_pi/agent/extensions/default-tools.ts
@@ -0,0 +1,18 @@
+/**
+ * Enable All Built-in Tools
+ *
+ * Activates grep (rg) and find (fd) tools on session start,
+ * in addition to the default read, bash, edit, write.
+ */
+
+import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
+
+export default function defaultTools(pi: ExtensionAPI) {
+  pi.on("session_start", () => {
+    const current = new Set(pi.getActiveTools());
+    current.add("grep");
+    current.add("find");
+
+    pi.setActiveTools(Array.from(current));
+  });
+}
