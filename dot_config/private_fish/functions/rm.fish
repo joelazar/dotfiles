cc52947d577f266ae60f7a5a1bf21c6a9473ac7d
joelazar <jozsef.gabor.lazar@gmail.com>
2026-04-28 13:10:06 +0200

    :wrench: refactor trash handling with native commands
    - Refactor rm.fish: switch to `trash` command with proper argument
      parsing to ignore flags
    - Update custom-update: use AppleScript to empty Trash via Finder
      instead of rm -rf
    - Remove .local/share/Trash cleanup (macOS-only now)

--- dot_config/private_fish/functions/rm.fish
+++ dot_config/private_fish/functions/rm.fish
@@ -1,7 +1,17 @@
-function rm --description 'map rm to gio trash'
-    if string match -q -- '-*' $argv[1]
-        set argv[1] (string replace -a 'r' '' -- $argv[1])
+function rm --description 'map rm to macOS trash'
+    set -l args
+    set -l opts 1
+
+    for arg in $argv
+        test $opts -eq 1; and test "$arg" = --; and set opts 0; and continue
+        test $opts -eq 1; and string match -q -- '-*' "$arg"; and continue
+        set -a args $arg
     end
 
-    gio trash $argv
+    test (count $args) -gt 0; or begin
+        command trash --help
+        return 1
+    end
+
+    command trash $args
 end
